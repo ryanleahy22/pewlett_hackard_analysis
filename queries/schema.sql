218ad62a7aed299483e5ebe1dfc9f69ffb4f5c09
unknown <ryanaleahy15@gmail.com>
2022-02-15 17:52:12 -0800

    sql challenge deliverable 1

--- queries/schema.sql
+++ queries/schema.sql
@@ -35,4 +35,24 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+create table dept_emp (
+emp_no INT NOT NULL,
+dept_no varchar not null,
+from_date date not null,
+to_date int not null,
+foreign key (emp_no) references employees (emp_no),
+foreign key (dept_no) references departments (dept_no),
+primary key (emp_no, dept_no)
+);
+
+select * from dept_emp;
+
+create table titles (
+emp_no int not null,
+title varchar not null,
+from_date date not null,
+to_date date not null,
+foreign key (emp_no) references employees (emp_no),
+primary key (emp_no)
+);
 select * from departments;
