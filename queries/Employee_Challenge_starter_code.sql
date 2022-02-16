a71c975f49211873b4428ee9f63a31b88016cd29
unknown <ryanaleahy15@gmail.com>
2022-02-15 19:26:27 -0800

    complete sql challenge code and queries

--- queries/Employee_Challenge_starter_code.sql
+++ queries/Employee_Challenge_starter_code.sql
@@ -55,3 +55,19 @@ group by title
 order by count desc;
 
 select * from retiring_titles
+
+select distinct on (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+into mentorship_eligibility
+from employees as e
+left join dept_emp as de on e.emp_no = de.emp_no
+left join titles as t on e.emp_no = t.emp_no
+where de.to_date = '9999-01-01' and e.birth_date between '1965-01-01' and '1965-12-31'
+order by emp_no;
+
+select * from mentorship_eligibility
